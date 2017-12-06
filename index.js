404fd02817317740769a4d38ce4caedb3212f4ea
Nataly Shrits <nataly87s@gmail.com>
2017-12-06 21:12:06 +0200

    cli that create a Slim.js boilerplate

---
+++ index.js
@@ -0,0 +1,266 @@
+'use strict';
+
+const validateProjectName = require('validate-npm-package-name');
+const chalk = require('chalk');
+const commander = require('commander');
+const fs = require('fs-extra');
+const path = require('path');
+const spawn = require('cross-spawn');
+const semver = require('semver');
+
+const packageJson = require('./package.json');
+
+const allDependencies = require('./dependencies.json');
+
+let projectName;
+
+const program = new commander.Command(packageJson.name)
+  .version(packageJson.version)
+  .arguments('<project-directory>')
+  .usage(`${chalk.green('<project-directory>')} [options]`)
+  .action(name => {
+    projectName = name;
+  })
+  .option('--verbose', 'print additional logs')
+  .allowUnknownOption()
+  .on('--help', () => {
+    console.log(`    Only ${chalk.green('<project-directory>')} is required.`);
+  })
+  .parse(process.argv);
+
+if (typeof projectName === 'undefined') {
+  console.error('Please specify the project directory:');
+  console.log(
+    `  ${chalk.cyan(program.name())} ${chalk.green('<project-directory>')}`
+  );
+  console.log();
+  console.log('For example:');
+  console.log(`  ${chalk.cyan(program.name())} ${chalk.green('my-slim-app')}`);
+  console.log();
+  console.log(
+    `Run ${chalk.cyan(`${program.name()} --help`)} to see all options.`
+  );
+  process.exit(1);
+}
+
+function printValidationResults(results) {
+  if (typeof results !== 'undefined') {
+    results.forEach(error => {
+      console.error(chalk.red(`  *  ${error}`));
+    });
+  }
+}
+
+createApp(
+  projectName,
+  program.verbose,
+);
+
+function createApp(name, verbose) {
+  const root = path.resolve(name);
+  const appName = path.basename(root);
+
+  checkAppName(appName);
+  fs.ensureDirSync(name);
+  if (!isSafeToCreateProjectIn(root, name)) {
+    process.exit(1);
+  }
+
+  console.log(`Creating a new Slim.js app in ${chalk.green(root)}.`);
+  console.log();
+
+  const packageJson = {
+    name: appName,
+    version: '0.1.0',
+    private: true,
+  };
+  fs.writeFileSync(
+    path.join(root, 'package.json'),
+    JSON.stringify(packageJson, null, 2)
+  );
+
+  const originalDirectory = process.cwd();
+  process.chdir(root);
+
+  run(root, appName, verbose, originalDirectory);
+}
+
+function install(root, verbose) {
+  return new Promise((resolve, reject) => {
+    let command = 'npm';
+    let args= [
+      'install',
+      '--save',
+      '--save-exact',
+      '--loglevel',
+      'error',
+    ].concat(allDependencies);
+
+    if (verbose) {
+      args.push('--verbose');
+    }
+
+    const child = spawn(command, args, { stdio: 'inherit' });
+    child.on('close', code => {
+      if (code !== 0) {
+        reject({
+          command: `${command} ${args.join(' ')}`,
+        });
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+function run(
+  root,
+  appName,
+  verbose,
+  originalDirectory
+) {
+  console.log('Installing packages. This might take a couple of minutes.');
+
+  install(root, verbose)
+    .then(() => {
+      setCaretRangeForRuntimeDeps();
+      const init = require('./init');
+      init(root, appName, verbose, originalDirectory);
+    })
+    .catch(reason => {
+      console.log();
+      console.log('Aborting installation.');
+      if (reason.command) {
+        console.log(`  ${chalk.cyan(reason.command)} has failed.`);
+      } else {
+        console.log(chalk.red('Unexpected error. Please report it as a bug:'));
+        console.log(reason);
+      }
+      console.log();
+
+      // On 'exit' we will delete these files from target directory.
+      const knownGeneratedFiles = [
+        'package.json',
+        'npm-debug.log',
+        'yarn-error.log',
+        'yarn-debug.log',
+        'node_modules',
+      ];
+      const currentFiles = fs.readdirSync(path.join(root));
+      currentFiles.forEach(file => {
+        knownGeneratedFiles.forEach(fileToMatch => {
+          // This will catch `(npm-debug|yarn-error|yarn-debug).log*` files
+          // and the rest of knownGeneratedFiles.
+          if (
+            (fileToMatch.match(/.log/g) && file.indexOf(fileToMatch) === 0) ||
+            file === fileToMatch
+          ) {
+            console.log(`Deleting generated file... ${chalk.cyan(file)}`);
+            fs.removeSync(path.join(root, file));
+          }
+        });
+      });
+      const remainingFiles = fs.readdirSync(path.join(root));
+      if (!remainingFiles.length) {
+        // Delete target folder if empty
+        console.log(
+          `Deleting ${chalk.cyan(`${appName} /`)} from ${chalk.cyan(
+            path.resolve(root, '..')
+          )}`
+        );
+        process.chdir(path.resolve(root, '..'));
+        fs.removeSync(path.join(root));
+      }
+      console.log('Done.');
+      process.exit(1);
+    });
+}
+
+function checkAppName(appName) {
+  const validationResult = validateProjectName(appName);
+  if (!validationResult.validForNewPackages) {
+    console.error(
+      `Could not create a project called ${chalk.red(
+        `"${appName}"`
+      )} because of npm naming restrictions:`
+    );
+    printValidationResults(validationResult.errors);
+    printValidationResults(validationResult.warnings);
+    process.exit(1);
+  }
+}
+
+function makeCaretRange(dependencies, name) {
+  const version = dependencies[name];
+
+  if (typeof version === 'undefined') {
+    console.error(chalk.red(`Missing ${name} dependency in package.json`));
+    process.exit(1);
+  }
+
+  let patchedVersion = `^${version}`;
+
+  if (!semver.validRange(patchedVersion)) {
+    console.error(
+      `Unable to patch ${name} dependency version because version ${chalk.red(
+        version
+      )} will become invalid ${chalk.red(patchedVersion)}`
+    );
+    patchedVersion = version;
+  }
+
+  dependencies[name] = patchedVersion;
+}
+
+function setCaretRangeForRuntimeDeps() {
+  const packagePath = path.join(process.cwd(), 'package.json');
+  const packageJson = require(packagePath);
+
+  if (typeof packageJson.dependencies === 'undefined') {
+    console.error(chalk.red('Missing dependencies in package.json'));
+    process.exit(1);
+  }
+
+  for (let dependency of allDependencies) {
+    makeCaretRange(packageJson.dependencies, dependency);
+  }
+
+  fs.writeFileSync(packagePath, JSON.stringify(packageJson, null, 2));
+}
+function isSafeToCreateProjectIn(root, name) {
+  const validFiles = [
+    '.DS_Store',
+    'Thumbs.db',
+    '.git',
+    '.gitignore',
+    '.idea',
+    'README.md',
+    'LICENSE',
+    'web.iml',
+    '.hg',
+    '.hgignore',
+    '.hgcheck',
+  ];
+  console.log();
+
+  const conflicts = fs
+    .readdirSync(root)
+    .filter(file => !validFiles.includes(file));
+  if (conflicts.length < 1) {
+    return true;
+  }
+
+  console.log(
+    `The directory ${chalk.green(name)} contains files that could conflict:`
+  );
+  console.log();
+  for (const file of conflicts) {
+    console.log(`  ${file}`);
+  }
+  console.log();
+  console.log(
+    'Either try using a new directory name, or remove the files listed above.'
+  );
+
+  return false;
+}
---
+++ init.js
@@ -0,0 +1,103 @@
+'use strict';
+
+// Makes the script crash on unhandled rejections instead of silently
+// ignoring them. In the future, promise rejections that are not handled will
+// terminate the Node.js process with a non-zero exit code.
+process.on('unhandledRejection', err => {
+  throw err;
+});
+
+const fs = require('fs-extra');
+const path = require('path');
+const chalk = require('chalk');
+const spawn = require('cross-spawn');
+
+const devDependencies = require('./devDependencies.json');
+
+module.exports = function(
+  appPath,
+  appName,
+  verbose,
+  originalDirectory
+) {
+  const appPackage = require(path.join(appPath, 'package.json'));
+
+  // Copy over some of the devDependencies
+  appPackage.dependencies = appPackage.dependencies || {};
+
+  // Setup the script rules
+  appPackage.scripts = {
+    build: 'webpack',
+    start: 'webpack-dev-server --watch --open --progress --colors --config webpack.config.js',
+  };
+
+  fs.writeFileSync(
+    path.join(appPath, 'package.json'),
+    JSON.stringify(appPackage, null, 2)
+  );
+
+  const readmeExists = fs.existsSync(path.join(appPath, 'README.md'));
+  if (readmeExists) {
+    fs.renameSync(
+      path.join(appPath, 'README.md'),
+      path.join(appPath, 'README.old.md')
+    );
+  }
+
+  // Copy the files for the user
+  const templatePath = path.join(__dirname, 'template');
+  if (fs.existsSync(templatePath)) {
+    fs.copySync(templatePath, appPath);
+  } else {
+    console.error(
+      `Could not locate supplied template: ${chalk.green(templatePath)}`
+    );
+    return;
+  }
+
+  // Rename gitignore after the fact to prevent npm from renaming it to .npmignore
+  // See: https://github.com/npm/npm/issues/1862
+  fs.move(
+    path.join(appPath, 'gitignore'),
+    path.join(appPath, '.gitignore'),
+    [],
+    err => {
+      if (err) {
+        // Append if there's already a `.gitignore` file there
+        if (err.code === 'EEXIST') {
+          const data = fs.readFileSync(path.join(appPath, 'gitignore'));
+          fs.appendFileSync(path.join(appPath, '.gitignore'), data);
+          fs.unlinkSync(path.join(appPath, 'gitignore'));
+        } else {
+          throw err;
+        }
+      }
+    }
+  );
+
+  let command = 'npm';
+  let args = ['install', '--save-dev', verbose && '--verbose'].filter(e => e).concat(devDependencies);
+  
+    console.log(`Installing dev dependencies using ${command}...`);
+    console.log();
+
+    const proc = spawn.sync(command, args, { stdio: 'inherit' });
+    if (proc.status !== 0) {
+      console.error(`\`${command} ${args.join(' ')}\` failed`);
+      return;
+    }
+
+  console.log();
+  console.log(`Success! Created ${appName} at ${appPath}`);
+  
+  if (readmeExists) {
+    console.log();
+    console.log(
+      chalk.yellow(
+        'You had a `README.md` file, we renamed it to `README.old.md`'
+      )
+    );
+  }
+  console.log();
+  console.log('Happy hacking!');
+};
